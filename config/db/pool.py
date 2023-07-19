a95f7cbdb9f1d1cfa011eb42e845ef2ceb8948dd
juan480ps <you@example.comjcorrea480@outlook.com>
2023-07-19 14:23:55 -0400

    a

--- config/db/pool.py
+++ config/db/pool.py
@@ -1,20 +1,20 @@
 import cx_Oracle, logging, yaml
 
-config_file = 'config/db/db_config_orcl.yaml'
+config_file = 'config/db/db_config_orcl.yaml' # se lee el archivo config
 file = open(config_file, 'r')
 config_data = file.read()
 file.close()
-config = yaml.safe_load(config_data)
+config = yaml.safe_load(config_data) # se carga la configuracion
 
 username = config['username']
 password = config['password']
 sdn = f"{config['host']}:{config['port']}/{config['sid']}"
 
-pd_pool = cx_Oracle.SessionPool(username, password, sdn, min = 2, max = 2, increment = 1, threaded = True)
+pd_pool = cx_Oracle.SessionPool(username, password, sdn, min = 5, max = 10, increment = 1, threaded = True)# se define el pool de conexiones 
 
-logging.debug("Conectada BD JDE PD")
+logging.debug("Conectada BD JDE PD") # logging de prueba
 
-def getCursorJDE():
+def getCursorJDE(): # se define una funcion para abrir conexion y cursor del jde
     con = pd_pool.acquire()
     cur = con.cursor()
     return cur
