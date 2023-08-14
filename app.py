4fd6638f752af4e25657553a24289f38ad96cf7e
juan480ps <you@example.comjcorrea480@outlook.com>
2023-08-14 08:02:58 -0400

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 from api import app #modulo principal que se usa para la entrada wsgi
 
 if __name__=='__main__':
-    app.run(debug=False, threaded=True, port=6000)
+    app.run(host='0.0.0.0', debug=False, threaded=True, port=6000)
